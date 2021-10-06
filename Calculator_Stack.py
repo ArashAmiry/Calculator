a60146e6600dc76fea0c84bea06ff57694efdb27
Arash Amiry <ArashAmiry@hotmail.com>
2021-10-06 13:54:22 +0200

    Version 1 of Calculator
    Not working. Continue to create a working stack.

---
+++ Calculator_Stack.py
@@ -0,0 +1,43 @@
+# Python program to convert infix expression to postfix
+
+# Class to convert the expression
+class Stack:
+
+    # Constructor to initialize the class variables
+    def __init__(self, capacity):
+        self.top = -1
+        self.capacity = capacity
+        # This array is used a stack
+        self.array = []
+        # Precedence setting
+        self.output = []
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+
+    # check if the stack is empty
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    # Return the value of the top of the stack
+    def peek(self):
+        return self.array[-1]
+
+    # Pop the element from the stack
+    def pop(self):
+        if not self.isEmpty():
+            self.top -= 1
+            return self.array.pop()
+        else:
+            return "$"
+
+    # Push the element to the stack
+    def push(self, op):
+        self.top += 1
+        self.array.append(op)
+
+    # A utility function to check is the given character
+    # is operand
+    def isOperand(self, ch):
+        return ch.isalpha()
